4d9027a9e895e1c5b60417d02960b9076b060c3e
John J. Allison <jja@sinequanon.net>
2021-02-01 10:35:54 -0700

    cleanup code, fix icon pct again

--- battery-indicator.sh
+++ battery-indicator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage: battery-indicator.sh [-m max_retries]
+
 SCALE=24
 
 ICON_DIR=$(dirname "$0")"/icons/"$SCALE"/"
@@ -7,9 +9,9 @@ ICON_EXT="svg"
 ICON_PREFIX="battery"
 DEFICON=$ICON_DIR/battery-missing.svg
 
-# try to get data several times
 count=0
 max=10
+
 if [ "$1" = "-m" ]
 then
     shift;
@@ -19,89 +21,84 @@ then
         shift;
     fi
 fi
+
+# try to get data several times
 while [ $count -lt $max ]
 do
-  if [ $count -gt 0 ]
-  then
-    sleep 1
-  fi
-  count=$(( $count + 1 ))
+    if [ $count -gt 0 ]
+    then
+        sleep 1
+    fi
+    count=$(( $count + 1 ))
 
-# ###
-# ### begin original script code
 
-# To find out: `upower -d | grep /battery`
-BATTERY_NAME="/org/freedesktop/UPower/devices/battery_BAT0"
+    # To find out: `upower -d | grep /battery`
+    BATTERY_NAME="/org/freedesktop/UPower/devices/battery_BAT0"
 
-read -N256 REQ <<<$(upower -i $BATTERY_NAME | grep -E "state|to\ full|time\ to\ empty|percentage" )
+    grepexpr="state:|time to|percentage"
+    read -N256 REQ <<<$(upower -i $BATTERY_NAME | grep -E "$grepexpr" )
 
-# example REQ values:
-#  state: charging time to full: 49.5 minutes percentage: 54%
-#  state: discharging time to empty: 24.3 minutes percentage: 8%
+    # example REQ values:
+    #  state:               fully-charged
+    #  state: charging time to full: 49.5 minutes percentage: 54%
+    #  state: discharging time to empty: 24.3 minutes percentage: 8%
 
-if [ -z "$REQ" ]
-then
-  continue
-fi
+    if [ -z "$REQ" ]
+    then
+        continue
+    fi
 
-arrState=($REQ)
+    arrState=($REQ)
+    AC=${arrState[1]}
+    ETA=${arrState[5]}\ ${arrState[6]}
+    PERCENTAGE=${arrState[8]//%/}
+    #echo "AC $AC ETA $ETA PERCENTAGE $PERCENTAGE" 1>&2
 
-AC=${arrState[1]}
-ETA=${arrState[5]}\ ${arrState[6]}
-PERCENTAGE=${arrState[8]//%/}
+    if [ 0"$PERCENTAGE" -lt 10 ]
+    then
+        PERCENT_ICON="000"
+    elif [ 0"$PERCENTAGE" -gt 98 ]
+    then
+        PERCENT_ICON="100"
+    else
+        PERCENT_ICON="0"${PERCENTAGE:0:1}"0"
+    fi
 
-if [ $PERCENTAGE -lt 10 ]
-then
- PERCENT_ICON="000"
-else
- PERCENT_ICON="0"${PERCENTAGE:0:1}"0"
-fi
+    case $AC in
+        "charging") SUFFIX="-charging" ;;
+        "discharging") SUFFIX="" ;;
+        "fully-charged")
+            SUFFIX="charged"
+            PERCENT_ICON=""
+            ;;
+    esac
 
-# echo $(( 10#$x ))
+    ICON=$ICON_DIR$ICON_PREFIX-$PERCENT_ICON$SUFFIX.$ICON_EXT
 
-case $AC in
-    "charging") SUFFIX="-charging" ;;
-    "discharging") SUFFIX="" ;;    
-    "fully-charged") SUFFIX="" ;;
-esac
+    if [ ! -e "$ICON" ]
+    then
+        echo "$0" "no icon, continuing to try again..." 1>&2
+        echo "REQ=$REQ" 1>&2
+        echo "AC=$AC" 1>&2
+        echo "PERCENTAGE=$PERCENTAGE" 1>&2
+        echo "ICON=$ICON" 1>&2
+        continue
+    fi
 
-case $PERCENTAGE in
-    [0-9]|[1-9][0-9]) PCT=$PERCENT_ICON ;;
-    100) PCT="100" ;;
-    *) PCT="charged" ;;
-esac
+    echo "<img>$ICON</img>"
 
-ICON=$ICON_DIR$ICON_PREFIX-$PCT$SUFFIX.$ICON_EXT 
+    echo -n "<tool><b>${AC^}"
+    if [ "$AC" != "fully-charged" ]
+    then
+        echo " ($PERCENTAGE%)"
+        echo -n "$ETA left."
+    fi
+    echo "</b></tool>"
 
-if [ ! -e "$ICON" ]
-then
-  echo "$0" "no icon, continuing to try again..." 1>&2
-  echo "REQ=$REQ" 1>&2
-  echo "AC=$AC" 1>&2
-  echo "PERCENTAGE=$PERCENTAGE" 1>&2
-  echo "ICON=$ICON" 1>&2
-  continue
-fi
+    echo "<click>xfce4-power-manager-settings</click>"
 
-echo "<img>$ICON</img>"
+    exit 0
 
-case $AC in
-    "fully-charged")
-    echo "<tool><b>${AC^}</b></tool>"
-;;
-
-    *)
-    echo "<tool><b>${AC^} ($PERCENTAGE%)"
-    echo "$ETA left.</b></tool>"
-;;    
-esac
-
-echo "<click>xfce4-power-manager-settings</click>"
-
-exit 0
-
-# ### end original script code
-# ###
 done
 
 # loop ended with no good data found, output something
